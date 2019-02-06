e11c7a3246b9ac4d8ba3f4500b130d055fe895c3
Mumu3w <mumu3w@outlook.com>
2019-02-06 22:05:53 +0800

    update minix1.3_tc2_tasm _image.bat

--- minix1.3_tc2_tasm/tools/_image.bat
+++ minix1.3_tc2_tasm/tools/_image.bat
@@ -1,4 +1,4 @@
 rem 
 build bootblok ..\kernel\kernel.out ..\mm\mm.out ..\fs\fs.out init.out fsck.out %1
 rem
-build bootblok ..\kernel\kernel.sep ..\mm\mm.sep ..\fs\fs.sep init.sep fsck.out %1.SEP
+build bootblok ..\kernel\kernel.sep ..\mm\mm.sep ..\fs\fs.sep init.sep fsck.sep %1.SEP
